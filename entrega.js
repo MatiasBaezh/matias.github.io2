986f53968b6bed7bc5110c709be03d23b6b09172
someone <someone@someplace.com>
2022-12-12 21:32:11 -0300

    segunda entrega, agregados objetos, array, filtro mediante extraccion de indice

--- entrega.js
+++ entrega.js
@@ -1,4 +1,14 @@
- // Monto del crédito
+ 
+ 
+ //Objeto  cliente
+const Cliente = {
+    nombre: prompt("¿Cual es tu nombre?"),
+    edad: prompt("¿Que edad tienes?"),
+    domicilio: prompt("¿Cuál es tu dirección?"),
+  }
+
+
+// Monto del crédito
  function monto1(){
     let monto1 = parseInt(prompt("Ingresar monto que deseas pagar en cuotas."));
     alert("El monto total a pagar es de " + monto1 +" dolares" )
@@ -14,13 +24,6 @@
 let monto = monto1()
 let cuotas = cuotas1()
 
-//chequear que variable cuotas sea mayor a 0
-if (cuotas <=1 ) {
-    alert("ingrese un numero mayor a cero.")
-  } else {
-    alert("El monto total se pagará en " + cuotas + " cuotas mensuales.") 
-  }
-
 
 //Funcion para dividir monto en cuotas
 function dividir(dato1, dato2){
@@ -28,7 +31,24 @@ function dividir(dato1, dato2){
    return resultado
 }
 
-let montocuota = dividir(monto, cuotas)
+let montocuota = dividir(monto, cuotas);
+
 
 //Mostrar resultado
-alert("Pagarás " + cuotas + " cuotas mensuales de " + montocuota + " dolares cada una.")
+alert(Cliente.nombre + " de" + Cliente.edad + " años de edad, con domicilio en " + Cliente.domicilio + " Pagará " + monto + " en " + cuotas + " cuotas mensuales de " + montocuota + " dolares cada una.")
+
+const Aval = {
+  nombre: prompt("¿Cual es el nombre del aval?"),
+  edad: prompt("¿Que edad tiene el aval?"),
+}
+
+const Riesgo = [ ];
+Riesgo.push(Aval.edad);
+console.log(Riesgo);
+
+if (Riesgo[0] <= 25) {
+  console.log("Riesgo alto")
+}
+  else {
+    console.log ("cliente sin riesgo")
+  }
