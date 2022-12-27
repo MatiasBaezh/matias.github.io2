6ccea91ac097105f10b13e9cf1240252d2c66eb1
someone <someone@someplace.com>
2022-12-26 22:37:12 -0300

    Agrego Eventos, DOM  con queryselector e InnerHTML

--- entrega.js
+++ entrega.js
@@ -1,56 +1,35 @@
- 
- 
- //Objeto  cliente
-const Cliente = {
-    nombre: prompt("¿Cual es tu nombre?"),
-    edad: prompt("¿Que edad tienes?"),
-    domicilio: prompt("¿Cuál es tu dirección?"),
+
+// Crear un objeto mediante un evento y almacena los datos ingresados en el formulario
+const form = document.querySelector('#Formulario');
+
+form.addEventListener('submit', (event) => {
+  // Evita recargar la página
+  event.preventDefault();
+
+  // Crear un objeto FormData a partir del formulario
+  const formData = new FormData(form);
+
+  // Crea un objeto vacío para almacenar los datos del formulario
+  const datos = {};
+
+  // Itera para guardar los datos del formulario en el objeto que esta vacío
+  for (const [key, value] of formData.entries()) {
+    datos[key] = value;
   }
 
-
-// Monto del crédito
- function monto1(){
-    let monto1 = parseInt(prompt("Ingresar monto que deseas pagar en cuotas."));
-    alert("El monto total a pagar es de " + monto1 +" dolares" )
-    return monto1
-}
-
-//Cantidad de cuotas
-    function cuotas1(){
-        let cuotas1 = parseInt(prompt(" Elije la cantidad de cuotas  mensuales."));
-       return cuotas1   
-    }
-       
-let monto = monto1()
-let cuotas = cuotas1()
-
-
-//Funcion para dividir monto en cuotas
-function dividir(dato1, dato2){
-   let resultado = dato1 / dato2;
-   return resultado
-}
-
-let montocuota = dividir(monto, cuotas);
-
-
-//Mostrar resultado
-alert(Cliente.nombre + " de" + Cliente.edad + " años de edad, con domicilio en " + Cliente.domicilio + " Pagará " + monto + " en " + cuotas + " cuotas mensuales de " + montocuota + " dolares cada una.")
-
-//Crea objeto "aval" y captura los parametros mediante prompt
-const Aval = {
-  nombre: prompt("¿Cual es el nombre del aval?"),
-  edad: prompt("¿Que edad tiene el aval?"),
-}
-//Crea array "Riesgo" pushea parametro de edad del aval.
-const Riesgo = [ ];
-Riesgo.push(Aval.edad);
-console.log(Riesgo);
-
-//compara la posición 0 del array mediante un ciclo para filtrar el riesgo.
-if (Riesgo[0] <= 25) {
-  console.log("Riesgo alto")
-}
-  else {
-    console.log ("cliente sin riesgo")
+  // Funcion que divide el monto por la cantidad de cuotas
+  function dividir(datos) {
+    
+    return datos.monto / datos.cuotas;
   }
+  
+  //Resultado de la division se guarda en una variable
+  const resultado = dividir(datos);
+
+  // Caputuro el parrafo en html donde quiero mostrar el resultado de la funció
+  const elementoResultado = document.querySelector('#resultado');
+
+  // Muestro el resultado de la división modificando el contenido del elemento HTML
+  elementoResultado.innerHTML = resultado;
+});
+
